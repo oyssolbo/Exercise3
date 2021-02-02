ff17579c219ff3078319109dc33e8b9b04b01995
Øystein Solbø <oyssolbo@stud.ntnu.no>
2021-02-02 14:18:59 +0100

    Created some raw modules to play with
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

---
+++ Elixir/UDP/udp/lib/udp.ex
@@ -0,0 +1,18 @@
+defmodule Udp do
+  @moduledoc """
+  Documentation for `Udp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Udp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
